9fd4ed070fcbc668914408d1f6862710da1f7fd8
Gaurav Shukla <gauravshukla156@gmail.com>
2024-09-22 13:46:28 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict" // treat all JS as new version
+
+// alert(2 + 3) // we are using node.js, not browser
+
+console.log(3 
+    + 3)    // code readabilty should be high
+
+console.log(2 + 5); console.log("Gaurav") // use semi-colon for line break
+
+
+let name = "Gaurav"
+let age = 26
+let isLoggedIn = false
+let state;
+
+/*
+PRIMITIVE TYPES
+
+number => 2 to power of 53
+bigInt
+string => ""
+boolean => true/false
+null => standalone value
+undefined => eg. let state;
+symbol => unique
+
+NON PRIMITIVE TYPE
+
+object
+*/
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+
