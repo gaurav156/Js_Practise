599eef6c508f19d1a78b98191a995f2067f4de6d
Gaurav Shukla <gauravshukla156@gmail.com>
2024-09-23 14:29:13 +0530

    Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -30,3 +30,44 @@ let boolType = Boolean(0);
 "" => false
 "hitesh" => true
 */
+
+// *********************** Operations ***********************
+
+let num = 3
+// console.log(-num);
+// console.log(-true); // -1
+// console.log(- + ""); // -0, tries to conevert it from Boolean
+// console.log(- + "a"); //NaN
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); // 2 raise to power of 2
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(+true); // +1
+// console.log(+""); // 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+console.log(++gameCounter);
+gameCounter = 100
+console.log(gameCounter++);
+
