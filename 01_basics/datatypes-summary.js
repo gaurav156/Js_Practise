3e046b670a9ce72091b47cc19fce88e2607c8d41
Gaurav Shukla <gauravshukla156@gmail.com>
2024-09-23 16:13:00 +0530

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,43 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id) // both the o/p are same...Symbol(123)
+console.log(anotherId)
+
+console.log(id === anotherId); // false, not same.
+
+const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function() {
+    console.log("Hello world");
+}
+
+// type of all Non primitive data types are object
+console.log(typeof myFunction); // object function
+console.log(typeof anotherId); // symbol
+console.log(typeof bigNumber); // bigint
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
