4b1a4f20071d026ccb30b31f1bbc3f87598c3e89
gaurav156 <gauravshukla156@gmail.com>
2024-09-23 13:00:37 +0530

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = "33";
+
+// console.log(score);
+// console.log(typeof score);
+
+let numberType = Number(score);
+
+// console.log(numberType);
+// console.log(typeof numberType);
+
+// let numberType1 = Number("22x"); // NaN - Not a Number
+// let numberType1 = Number(null); // 0
+let numberType1 = Number(undefined); // NaN - Not a Number
+
+// console.log(numberType1);
+// console.log(typeof numberType1);
+
+let stringType = String(43);
+
+// console.log(stringType);
+// console.log(typeof stringType);
+
+let boolType = Boolean(0);
+
+// console.log(boolType);
+// console.log(typeof boolType);
+
+/*
+1 => true; 0 => false
+"" => false
+"hitesh" => true
+*/
