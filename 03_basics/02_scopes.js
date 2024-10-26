316c6fe933e149d59c1ace3f00e558b020867676
Gaurav Shukla <gauravshukla156@gmail.com>
2024-10-26 21:58:53 +0530

    scope this and arrow

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,56 @@
+//var c = 300 // --> globally accessible
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+addone(5) // 6
+function addone(num) {
+    console.log(num + 1)
+}
+
+
+addTwo(5) // Cannot access 'addTwo' before initialization
+const addTwo = function(num) {
+    console.log(num + 2)
+}
